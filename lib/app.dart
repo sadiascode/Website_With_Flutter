573caf50382eaae9e63bcea706edb205c8adec2b
Sadiascode <rahmanrohasadia@gmail.com>
2026-03-03 14:35:16 +0600

    Web With Flutter

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FoodService extends StatelessWidget {
+  const FoodService({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: "Food Service",
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:foodservice/app.dart';
+
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const FoodService ());
+}
